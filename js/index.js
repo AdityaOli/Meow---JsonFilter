23034a419bb32c93d65edf5a3d2424ba667661f0
AdityaOli <adityaoligehu@gmail.com>
2017-11-12 22:33:49 +0530

    Added stacking

--- js/index.js
+++ js/index.js
@@ -1,11 +1,11 @@
 var containsList = [];
-var parentChildStack = "";
+var separatorStack = [];
 $("#customSearch").on("click",
   function()
   {
     var jsonString = $("#inputJson").val();
     var json = JSON.parse(jsonString);
-    var text = setKeys(json);
+    setKeys(json);
   }
 );
 
@@ -19,10 +19,10 @@ function setKeys(jsonData)
     {
       if(containsList.indexOf(jsonKeys[key])<0)
       {
-          $("#Output").append("<a class='btn btn-primary' id="+jsonKeys[key]+">"+jsonKeys[key]+"</a>");
-          containsList.push(jsonKeys[key]);
-          //parentChildStack += jsonKeys[key] + "->";
-          //console.log(parentChildStack);
+        separatorStack.push(jsonKeys[key]);
+        console.log(separatorStack);
+        $("#Output").append("<a class='btn btn-primary' id="+separatorStack.join("/")+">"+separatorStack.join("/")+"</a>");
+        //containsList.push(jsonKeys[key]);
       }
       setKeys(jsonData[jsonKeys[key]]);
     }
@@ -31,81 +31,39 @@ function setKeys(jsonData)
       var array = jsonData[jsonKeys[key]];
       if(containsList.indexOf(jsonKeys[key])<0)
       {
-          $("#Output").append("<a class='btn btn-primary' id="+jsonKeys[key]+">"+jsonKeys[key]+"</a>");
-          containsList.push(jsonKeys[key]);
-          //parentChildStack += jsonKeys[key] + "->";
-          //console.log(parentChildStack);
+        separatorStack.push(jsonKeys[key]);
+        console.log(separatorStack);
+        $("#Output").append("<a class='btn btn-primary' id="+separatorStack.join("/")+">"+separatorStack.join("/")+"</a>");
+        //containsList.push(jsonKeys[key]);
       }
-      for(var i=0;i<array.length;i++)
+
+      if(isObject(array[0]))
       {
-        if(isObject(array[i]))
-        {
-          //if(containsList.indexOf(array[i])<0)
-          //    $("#Output").append("<a class=btn btn-primary' id="+array[i]+">"+array[i]+"</a>");
-          setKeys(array[i]);
-        }
+          setKeys(array[0]);
+          //separatorStack.push(jsonKeys[key]);
+          console.log(separatorStack);
       }
     }
     else
     {
       if(containsList.indexOf(jsonKeys[key])<0)
       {
-          $("#Output").append("<a class='btn btn-primary' id="+jsonKeys[key]+">"+jsonKeys[key]+"</a>");
-          containsList.push(jsonKeys[key]);
-          //parentChildStack += jsonKeys[key] + "->";
-          //console.log(parentChildStack);
+        separatorStack.push(jsonKeys[key]);
+        console.log(separatorStack);
+        $("#Output").append("<a class='btn btn-primary' id="+separatorStack.join("/")+">"+separatorStack.join("/")+"</a>");
+        //containsList.push(jsonKeys[key]);
       }
     }
   }
-  var tempList = parentChildStack.split("->");
-  tempList.r
-  //parentChildStack = tempList.join("->");
-  ///console.log(parentChildStack);
+  //console.log(separatorStack);
+  //separatorStack.pop();
 }
+
+//===================================== Check Fields ==================================================================
 function isObject(value) {
   return value && typeof value === 'object' && value.constructor === Object;
 };
-
-function isString(value) {
-  return typeof value === 'string' || value instanceof String;
-};
-
-function isNumber(value) {
-  return typeof value === 'number' && isFinite(value);
-};
-
 function isArray(value) {
   return value && typeof value === 'object' && value.constructor === Array;
 };
-
-function isFunction(value) {
-  return typeof value === 'function';
-};
-
-function isNull(value) {
-  return value === null;
-};
-
-function isUndefined(value) {
-  return typeof value === 'undefined';
-};
-
-function isBoolean(value) {
-  return typeof value === 'boolean';
-};
-
-function isRegExp(value) {
-  return value && typeof value === 'object' && value.constructor === RegExp;
-};
-
-function isError(value) {
-  return value instanceof Error && typeof value.message !== 'undefined';
-};
-
-function isDate(value) {
-  return value instanceof Date;
-};
-
-function isSymbol(value) {
-  return typeof value === 'symbol';
-};
+//================================================End Check Fields=======================================================
